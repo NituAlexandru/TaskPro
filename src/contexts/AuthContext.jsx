6af79ff660568991897581b4325334c323375bf2
OxS90 <oxana.slivinschi@gmail.com>
2024-07-21 15:13:54 +0300

    AuthContext and AuthCallbach changed for googleAuth to work

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,15 +1,15 @@
-import { createContext, useState, useEffect, useCallback } from "react";
-import PropTypes from "prop-types";
-import { register, login, fetchCurrentUser } from "../service/authService";
+import { createContext, useState, useEffect, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import { register, login as apiLogin, fetchCurrentUser } from '../service/authService';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [token, setToken] = useState(localStorage.getItem("token") || null);
+  const [token, setToken] = useState(localStorage.getItem('token') || null);
 
   useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedUser = JSON.parse(localStorage.getItem('user'));
     console.log("Stored user:", storedUser);
     console.log("Stored token:", token);
     if (storedUser && token) {
@@ -21,30 +21,38 @@ export const AuthProvider = ({ children }) => {
     const data = await register(userData);
     setUser(data.user);
     setToken(data.token);
-    localStorage.setItem("user", JSON.stringify(data.user));
-    localStorage.setItem("token", data.token);
+    localStorage.setItem('user', JSON.stringify(data.user));
+    localStorage.setItem('token', data.token);
+    localStorage.setItem('refreshToken', data.refreshToken);
+    localStorage.setItem('sessionId', data.sid);
     console.log("Registered user:", data.user);
     console.log("Stored token after registration:", data.token);
     return data;
   };
 
   const loginUser = async (userData) => {
-    const data = await login(userData);
-    setUser(data.user);
-    setToken(data.token);
-    localStorage.setItem("user", JSON.stringify(data.user));
-    localStorage.setItem("token", data.token);
-    console.log("Logged in user:", data.user);
-    console.log("Stored token after login:", data.token);
+    const data = await apiLogin(userData);
+    login(data.user, data.token, data.refreshToken, data.sid);
     return data;
+  };
+
+  const login = (userData, token, refreshToken, sessionId) => {
+    console.log('Logging in user:', userData);
+    setUser(userData);
+    setToken(token);
+    localStorage.setItem('user', JSON.stringify(userData));
+    localStorage.setItem('token', token);
+    localStorage.setItem('refreshToken', refreshToken);
+    localStorage.setItem('sessionId', sessionId);
   };
 
   const logout = useCallback(() => {
     setUser(null);
     setToken(null);
-    localStorage.removeItem("user");
-    localStorage.removeItem("token");
-    localStorage.removeItem("refreshToken")
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('sessionId');
     console.log("Logged out");
   }, []);
 
@@ -67,9 +75,7 @@ export const AuthProvider = ({ children }) => {
   }, [token, logout]);
 
   return (
-    <AuthContext.Provider
-      value={{ user, token, registerUser, loginUser, logout, getCurrentUser }}
-    >
+    <AuthContext.Provider value={{ user, token, registerUser, loginUser, login, logout, getCurrentUser }}>
       {children}
     </AuthContext.Provider>
   );
