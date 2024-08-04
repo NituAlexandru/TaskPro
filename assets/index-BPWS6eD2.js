5269fc12580824d755a38888aaab93ea29eb0a38
Nitu Alexandru <nitzu_alexandru@yahoo.com>
2024-08-04 18:12:12 +0300

    Updates

--- assets/index-BPWS6eD2.js
+++ assets/index-BPWS6eD2.js
@@ -559,8 +559,9 @@ attempted value: ${o}
   background-color: ${({theme:e})=>e.body};
   box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
   z-index: 1;
-  min-width: 160px;
+  min-width: 140px;
   right: 0;
+  border-radius: 8px;
 
   & button {
     color: ${({theme:e})=>e.text};
@@ -568,7 +569,13 @@ attempted value: ${o}
     text-decoration: none;
     display: block;
     margin: 0;
-    padding: 0;
+    padding: 10px;
+    width: 100%;
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    letter-spacing: -0.02em;
+   
   }
 
   & button:hover {
